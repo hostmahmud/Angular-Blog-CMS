6e6f4a9afc24dd4580ba7fd7d14acaed417c1e0b
unknown <host.mahmud@gmail.com>
2021-09-21 18:33:37 +0600

    add database model files

---
+++ Database-Models/Author.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlogApi.Models
+{
+    public class Author
+    {
+        public Author()
+        {
+            //this.Posts = new HashSet<Post>();
+        }
+
+        [Key]
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+        public DateTime Dob { get; set; }
+        public string About { get; set; }
+
+        //nav
+        //public ICollection<Post> Posts { get; set; }
+    }
+}
---
+++ Database-Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlogApi.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            //this.Posts = new HashSet<Post>();
+        }
+        [Key]
+        public int CategoryId { get; set; }
+        public string Title { get; set; }
+
+        //nav
+        //public ICollection<Post> Posts { get; set; }
+    }
+}
